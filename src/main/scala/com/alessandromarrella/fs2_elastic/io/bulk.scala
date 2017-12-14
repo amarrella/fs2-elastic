16c1ada1e0bcf97ff33ab512c78a925c0c67efff
Alessandro Marrella <zioale91@gmail.com>
2017-12-14 19:07:59 +0000

    Info (#5)
    * Added versioning info
    
    * Added info api and header parameters to all apis

--- src/main/scala/com/alessandromarrella/fs2_elastic/io/bulk.scala
+++ src/main/scala/com/alessandromarrella/fs2_elastic/io/bulk.scala
@@ -1,17 +1,16 @@
 package com.alessandromarrella.fs2_elastic.io
 
 import fs2.Pipe
-import org.elasticsearch.action.bulk.{
-  BulkRequest,
-  BulkResponse
-}
+import org.apache.http.Header
+import org.elasticsearch.action.bulk.{BulkRequest, BulkResponse}
 import org.elasticsearch.client.RestHighLevelClient
 
 private[io] trait bulk {
 
   def bulk[F[_]](
-      bulkRequest: BulkRequest): Pipe[F, RestHighLevelClient, BulkResponse] =
-    client => client.map(_.bulk(bulkRequest))
+      bulkRequest: BulkRequest,
+      headers: Header*): Pipe[F, RestHighLevelClient, BulkResponse] =
+    client => client.map(_.bulk(bulkRequest, headers:_*))
 
 }
 
