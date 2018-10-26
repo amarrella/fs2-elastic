43900e15f6a527ea7d59c51a7e4c4d65b2511a69
Alessandro Marrella <zioale91@gmail.com>
2018-10-26 20:14:58 +0100

    fs2 1.0 (#22)

--- src/main/scala/com/alessandromarrella/fs2_elastic/ElasticClient.scala
+++ src/main/scala/com/alessandromarrella/fs2_elastic/ElasticClient.scala
@@ -4,33 +4,19 @@ import org.elasticsearch.client.{
   RestClientBuilder,
   RestHighLevelClient
 }
-import fs2._
-import cats.effect.Sync
+import cats.effect._
 import org.apache.http.HttpHost
 
 object Client {
 
-  private[this] def buildClient(hosts: Seq[HttpHost]): (RestHighLevelClient, RestClient) = {
-    val restClient = RestClient.builder(hosts: _*).build()
-    val restHighLevelClient = new RestHighLevelClient(restClient)
-    (restHighLevelClient, restClient)
-  }
-
-  private[this] def buildClient(restClientBuilder: RestClientBuilder): (RestHighLevelClient, RestClient) = {
-    val restClient = restClientBuilder.build()
-    val restHighLevelClient = new RestHighLevelClient(restClient)
-    (restHighLevelClient, restClient)
-  }
-
   def fromHosts[F[_]](hosts: HttpHost*)(
-      implicit F: Sync[F]): Stream[F, RestHighLevelClient] =
+      implicit F: Sync[F]): Resource[F, RestHighLevelClient] =
     fromClientBuilder(RestClient.builder(hosts: _*))
 
   def fromClientBuilder[F[_]](restClientBuilder: RestClientBuilder)(
-      implicit F: Sync[F]): Stream[F, RestHighLevelClient] =
-    Stream.bracket(F.delay(buildClient(restClientBuilder)))(
-      c => Stream.emit(c._1),    // We use the high level client
-      c => F.delay(c._2.close()) // But we need to close the underlying low level client when we are done
+      implicit F: Sync[F]): Resource[F, RestHighLevelClient] =
+    Resource.make(F.delay(restClientBuilder.build()))(c => F.delay(c.close())).flatMap(
+      restClient =>
+        Resource.liftF(F.delay(new RestHighLevelClient(restClient)))
     )
-
 }
