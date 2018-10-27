903c54e613e18db193ff311be938d4801a36c65e
amarrella <zioale91@gmail.com>
2018-10-27 12:39:41 +0100

    Reverting back to stream. Sorry for the inconvenience

--- src/main/scala/com/alessandromarrella/fs2_elastic/ElasticClient.scala
+++ src/main/scala/com/alessandromarrella/fs2_elastic/ElasticClient.scala
@@ -5,18 +5,19 @@ import org.elasticsearch.client.{
   RestHighLevelClient
 }
 import cats.effect._
+import fs2._
 import org.apache.http.HttpHost
 
 object Client {
 
   def fromHosts[F[_]](hosts: HttpHost*)(
-      implicit F: Sync[F]): Resource[F, RestHighLevelClient] =
+      implicit F: Sync[F]): Stream[F, RestHighLevelClient] =
     fromClientBuilder(RestClient.builder(hosts: _*))
 
   def fromClientBuilder[F[_]](restClientBuilder: RestClientBuilder)(
-      implicit F: Sync[F]): Resource[F, RestHighLevelClient] =
-    Resource.make(F.delay(restClientBuilder.build()))(c => F.delay(c.close())).flatMap(
+      implicit F: Sync[F]): Stream[F, RestHighLevelClient] =
+    Stream.bracket(F.delay(restClientBuilder.build()))(c => F.delay(c.close())).flatMap(
       restClient =>
-        Resource.liftF(F.delay(new RestHighLevelClient(restClient)))
+        Stream.eval(F.delay(new RestHighLevelClient(restClient)))
     )
 }
